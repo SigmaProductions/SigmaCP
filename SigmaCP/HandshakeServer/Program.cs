7108fe09392aac692615c8cd35a2ba14928fc279
senbar <barsen97@gmail.com>
2017-12-04 11:44:50 -0800

    working tcp

--- SigmaCP/HandshakeServer/Program.cs
+++ SigmaCP/HandshakeServer/Program.cs
@@ -7,29 +7,9 @@ namespace HandshakeServer
     {
         static void Main(string[] args)
         {
-            const string DEFAULT_SERVER = "192.168.88.1";
-            const int DEFAULT_PORT = 804;
-
-
-            static System.Net.Sockets.UdpClient udpServer = null;
-            static IPEndPoint serverEndPoint = null;
-
-            private static void Startup()
-            {
-                // The chat server always starts up on the localhost, using the default port 
-                IPHostEntry hostInfo = Dns.GetHostByName(DEFAULT_SERVER);
-                IPAddress serverAddr = hostInfo.AddressList[0];
-                serverEndPoint = new IPEndPoint(serverAddr, DEFAULT_PORT);
-                udpServer = new System.Net.Sockets.UdpClient();
-
-            }
-            public static int sendData(string data)
-            {
-                Startup();
-                udpServer.Send(System.Text.Encoding.ASCII.GetBytes(data), data.Length, serverEndPoint);
-                return 1;
-            }
+            var testc = new TCPGutClient();
+            testc.Send("dfasd");
         }
     }
-    }
+    
 }
--- SigmaCP/HandshakeServer/TCPGutClient.cs
+++ SigmaCP/HandshakeServer/TCPGutClient.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 
 namespace HandshakeServer
 {
     class TCPGutClient
     {
+        const string DEFAULT_SERVER = "192.168.88.219";
+        const int DEFAULT_PORT = 804;
+        TcpClient client;
+        public TCPGutClient()
+        {
+            client = new TcpClient();
+
+        }
+
+        public void Send(string data)
+        {
+            while (true)
+            {
+                try
+                {
+                    client.Connect(DEFAULT_SERVER, DEFAULT_PORT);
+                    break;
+                }
+                catch (Exception) { }
+            }
+
+            var tcpStream = client.GetStream();
+            tcpStream.Write(System.Text.Encoding.ASCII.GetBytes(data), 0,data.Length);
+            tcpStream.Close();
+        }
     }
 }
